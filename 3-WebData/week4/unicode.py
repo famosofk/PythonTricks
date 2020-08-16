fa0b525412e672bb486a7e5716be3dfb27e4c561
famosoFK <fabianogmjunior@gmail.com>
2020-08-16 12:08:03 -0300

    Update unicode.py

--- 3-WebData/week4/unicode.py
+++ 3-WebData/week4/unicode.py
@@ -4,5 +4,6 @@ import urllib.request, urllib.parse, urllib.error
 
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+#this returns us a file handler, like the handler provider by open() method
 for line in fhand:
     print(line.decode().strip())
