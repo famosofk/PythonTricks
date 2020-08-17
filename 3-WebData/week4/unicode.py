a20f1d9075aa83d0898be015ba4fe209ccfdde32
famosofk <fabianogmjunior@gmail.com>
2020-08-16 21:56:13 -0300

    daily commit

--- 3-WebData/week4/unicode.py
+++ 3-WebData/week4/unicode.py
@@ -1,8 +1,6 @@
 #since http is common, we have a lib that makes requests for us
 
 import urllib.request, urllib.parse, urllib.error
-
-
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 #this returns us a file handler, like the handler provider by open() method
 for line in fhand:
